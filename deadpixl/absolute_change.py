9b2251df74baea859fbe985c9325079c72a961cd
fran2402 <83206710+fran2402@users.noreply.github.com>
2025-06-20 17:21:35 +0200

    Delete deadpixl directory

--- deadpixl/absolute_change.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def absolute_change(a, b):
-    """ Find the change in the order of magnitude between two values."""
-    return np.abs(np.log10(abs(a)) - np.log10(abs(b)))
--- deadpixl/copy__init__.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from scipy.ndimage import binary_fill_holes, binary_erosion
-from scipy import ndimage
-from collections import deque
-from .absolute_change import absolute_change
-from .find_dead_pixels import *
-from .split_into_segments import split_into_segments
-from .interpolate_dead_pixels import interpolate_dead_pixels
-
